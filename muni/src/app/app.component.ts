c26d950df6d028d52d03273d0e6922872a333701
jjoacoc <165914437+jjoacoc@users.noreply.github.com>
2024-09-11 14:47:17 +0000

    Prueba1

--- muni/src/app/app.component.ts
+++ muni/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'muni';
+export class AppComponent implements OnInit {
+  datos: any[] = []; // Almacena los datos obtenidos
+  nuevoDato = {username: '', email: '', lastname: ''};
+
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.obtenerDatos();
+  }
+
+  obtenerDatos(): void{
+    this.dataService.obtenerDatos().subscribe(data => {
+      this.datos = data;
+    });
+  }
+
+  crearDatos():void{
+    this.dataService.crearDatos(this.nuevoDato).subscribe(()=> {
+      this.obtenerDatos();
+      this.nuevoDato = {username: '', email: '', lastname: ''};
+    })
+  }
+
+  eliminarDatos(id: number):void{
+    this.dataService.eliminarDatos(id).subscribe(()=>{
+      this.obtenerDatos();
+    })
+  }
 }
---
+++ muni/src/app/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost/crud.php'; // Cambia esto a la URL de tu script PHP
+
+  constructor(private http: HttpClient) { }
+
+  obtenerDatos(): Observable<any>{
+    return this.http.get(this.apiUrl);
+  }
+
+  crearDatos(datos:any): Observable<any>{
+    return this.http.post(this.apiUrl,datos);
+  }
+
+  eliminarDatos(id:number):Observable<any>{
+    return this.http.delete(`${this.apiUrl}?id=${id}`);
+ }
+
+
+}
